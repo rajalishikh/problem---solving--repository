e607e817b33c9e8e0e6dc06b0ea3b9d991fe8b25
unknown <nahidujjamanraj@gmail.com>
2024-10-01 11:44:47 +0600

    I am print even numbers from 1 to 40 and  continue to skip odd numbers

---
+++ 17th.js
@@ -0,0 +1,10 @@
+// Write a loop to print even numbers from 1 to 40. Use continue to skip odd numbers.
+for(let number=1; number<=40;number++){
+    if (number %2 !=0 ){
+        continue;
+
+
+    }else{
+        console.log(number)
+    }
+}
