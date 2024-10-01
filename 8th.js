db06856cf562a3da2b4a91fa93ed8f0295e36508
unknown <nahidujjamanraj@gmail.com>
2024-10-01 10:11:15 +0600

    I am make a multiplication table for number 5

---
+++ 8th.js
@@ -0,0 +1,9 @@
+// As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
+let number=1
+let multiplicationNumber=5
+console.log("Here is the multiplication table of 5  ")
+while(number<=10){
+    console.log(multiplicationNumber,"*", number,"=" , multiplicationNumber *number  )
+    number=number+1
+
+}
