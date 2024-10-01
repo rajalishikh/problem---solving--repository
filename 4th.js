0dc4dda7d895375ecf779a57105c4e47a0e04d5e
unknown <nahidujjamanraj@gmail.com>
2024-10-01 08:46:26 +0600

    I have extracted even  numbers from 78  to 98 using while loop

---
+++ 4th.js
@@ -0,0 +1,8 @@
+// Find all the even numbers from 78 to 98.
+let number =78
+while(number<=98){
+    if(number%2 == 0){
+        console.log('The even numbers from 78 t0 98 here it is ',number)
+    }
+    number=number+1; 
+}
