65b95c16cb0872782a5d6534f611d1b77568a772
unknown <nahidujjamanraj@gmail.com>
2024-10-01 11:52:38 +0600

    I am  display odd number from 55 to 85 and skip the numbers divisible by 5 .I solve this problem using by for loop

---
+++ 18th.js
@@ -0,0 +1,13 @@
+// display odd number from 55 to 85 and skip the numbers divisible by 5.
+for(let number=55; number<=85;number++){
+
+    if(number %5 == 0){
+       continue
+    }
+    else if(number %2 !=0 ) {
+        console.log(number)
+    }
+    
+        
+  
+}
