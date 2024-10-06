00e776c68c875a1c3b1e1e99806f1d8ea5159982
unknown <nahidujjamanraj@gmail.com>
2024-10-06 12:51:36 +0600

    Reverse practicing

--- bbb.js
+++ bbb.js
@@ -15,3 +15,17 @@ let  foodName='cake, chocolate  cake '
 
 console.log(foodName.slice(5,))
 
+let friendsName='Arif, raj, jihat, rasel, nibir, ornove, siam, baten, refat'
+console.log(friendsName.split(' ',))
+
+// practice 3 
+ let profession = 'I am a junior web developer'
+ let reverseSentence=' '
+
+ for(let reverse of profession){
+    reverseSentence =reverse + reverseSentence;
+    
+ }
+ console.log(reverseSentence)
+
+
