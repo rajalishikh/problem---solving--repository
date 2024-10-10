2416d7ffaeebd29ec719ea35993fbe9938f4edc4
unknown <nahidujjamanraj@gmail.com>
2024-10-10 10:08:05 +0600

    I am just add a property in obj

---
+++ 27.js
@@ -0,0 +1,9 @@
+// For this object below add a property named passenger capacity with value 5
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020
+};
+
+car['passenger capacity']= 5
+console.log(car)
