980b12cc8c22938fe5674e864ec5d7455be67e80
unknown <nahidujjamanraj@gmail.com>
2024-10-10 10:12:44 +0600

    I am just find out physics marks

---
+++ 28.js
@@ -0,0 +1,11 @@
+// Display the physics marks as output.
+const student = {
+    name: "Hamim Sakep",
+    id: 5421,
+    physics: {
+        subject: "HSC Physics",
+        author: "Shahjahan Tapan",
+        marks: 30
+    }
+};
+console.log("The physics marks Hamim Sakep here it is ",student.physics.marks)
