ec8ed5717274b5ddf0d3528515d2e200d30074ac
unknown <nahidujjamanraj@gmail.com>
2024-10-10 20:52:50 +0600

    I am Count how many times a string has the letter a

---
+++ 31.js
@@ -0,0 +1,8 @@
+// Count how many times a string has the letter a
+
+let sentenceAboutMySelf='My name is raj and I learn web development ';
+let match=sentenceAboutMySelf.match(/a/g).length
+console.log("The total number of a use is ",match)
+
+
+
