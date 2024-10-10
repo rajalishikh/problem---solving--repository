e12bde00e8339bee3ab11937f1c24a30a8bd4544
unknown <nahidujjamanraj@gmail.com>
2024-10-10 10:24:39 +0600

    I Count the number of properties in object

---
+++ 29.js
@@ -0,0 +1,10 @@
+// Count the number of properties
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+let countObject=Object.keys(student)
+let count=countObject.length
+console.log("Here the number of object keys here it is ",count )
