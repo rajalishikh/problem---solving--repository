9dac5aac0cc9050fe333aa1c74f8d4f3bcb87470
unknown <nahidujjamanraj@gmail.com>
2024-10-14 09:44:07 +0600

    In this program i run a for loop and find out all the value of array

--- 35.js
+++ 35.js
@@ -1,9 +1,10 @@
 // Capitalize Every first Letter of each word in a String
 
-let description_About_hometown='My home town is Dhaka, Dhaka is the Capital of Bangladesh.'
+let description_About_hometown='My home town is Dhaka, Dhaka is the Capital of Bangladesh'
+// console.log(description_About_hometown.length)
 
 let separate_each_word=description_About_hometown.split(' ')
-console.log(separate_each_word)
+console.log(separate_each_word.length)
 
 for (let i=0; i<separate_each_word.length; i++){
     separate_each_word[i]=separate_each_word[i][0].toUpperCase()+separate_each_word[i].slice(1)
---
+++ 37.js
@@ -0,0 +1,6 @@
+// Run for loop and while loop in a array and find the value of array 
+let name_of_Rich_people=['markjokirvar','alonmask','tata',]
+for(let i=0; i<name_of_Rich_people.length; i++){
+    console.log(name_of_Rich_people[i])
+}
+
