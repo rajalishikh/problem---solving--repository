00e53ffe46de44a5c8c301cdf66ed4a30380e14e
unknown <nahidujjamanraj@gmail.com>
2024-10-14 10:44:37 +0600

    I am reverse a string using two methods

---
+++ 40.js
@@ -0,0 +1,13 @@
+// practice reverse method in different ways 
+
+// use for loop to reverse a Array
+let number=[1,2,3,4,5,6,7,]
+let number_reverse=number.reverse()
+console.log(number_reverse)
+
+// reverse a string 
+let name_my_friend='raj nosib jihat arif rasel shopon'
+let reverse=name_my_friend.split(" ");
+reverse.reverse()
+console.log(reverse.join(' '))
+
