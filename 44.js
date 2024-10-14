05a3b956f4c2655faa0392d72032538b62ae9c73
unknown <nahidujjamanraj@gmail.com>
2024-10-14 21:38:16 +0600

    I am write a JavaScript code to get the even numbers from an array using any looping technique

---
+++ 44.js
@@ -0,0 +1,8 @@
+// Write a JavaScript code to get the even numbers from an array using any looping technique.
+const numbers = [12, 98, 5, 41, 23, 78, 46];
+for(let num of numbers){
+    if(num%2 == 0){
+        console.log("yes it is a even number ",num)
+    }
+   
+}
