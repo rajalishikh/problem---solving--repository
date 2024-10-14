7fefef008e3a82191a5f59fb76a6d0ebdc45e0cb
unknown <nahidujjamanraj@gmail.com>
2024-10-14 11:00:55 +0600

    In this program i reverse method using unshift method

---
+++ 41.js
@@ -0,0 +1,9 @@
+// reverse a array using for loop and unshift() method
+let name_friend =['Raj','jihat','nosib ']
+let reverse_name_friend=[]
+for(let name2 of name_friend){
+   reverse_name_friend.unshift(name2)
+    
+    
+}
+console.log(reverse_name_friend)
