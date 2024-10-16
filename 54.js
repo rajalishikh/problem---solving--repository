d2a987db7c65a8b3ab9cfc03d3c1828b6af1a96f
unknown <nahidujjamanraj@gmail.com>
2024-10-16 11:04:19 +0600

    I am write a function that returns all even addition elements of an array

---
+++ 54.js
@@ -0,0 +1,15 @@
+// Give me a function that returns all even addition elements of an array
+function addition_Even_Array(num){
+  let sum=0;
+    console.log(num)
+    for(let a of num){
+        if(a %2 == 0){
+            sum= sum +a;
+
+        }
+    }
+    return sum
+
+}
+let result =addition_Even_Array([12,3,6,8])
+console.log(result)
