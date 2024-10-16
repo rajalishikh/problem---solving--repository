a9135099b97afde79d158b9efcfd89b5365a8a36
unknown <nahidujjamanraj@gmail.com>
2024-10-16 09:46:44 +0600

    I am write function which give me answer that the number is even or odd

---
+++ 49.js
@@ -0,0 +1,15 @@
+function findEvenOrOdd(num){
+    if(num % 2 == 0){
+        console.log('The number is even ')
+        return true
+    }
+    else if(num % 2 != 0){
+        console.log('The number is odd ');
+        return true
+        
+
+    }
+
+}
+let result=findEvenOrOdd(23)
+console.log(result)
