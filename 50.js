e8cce6420f78ae1ff85cca9a5155d491dd2a8d7b
unknown <nahidujjamanraj@gmail.com>
2024-10-16 09:54:12 +0600

    I am write a funtion that returns me that the element of value even or odd

---
+++ 50.js
@@ -0,0 +1,12 @@
+function findLength(num1){
+    if(num1.length %2 == 0){
+        return true
+    }else if(num1.length % 2 != 0){
+        return false
+    }
+
+
+
+}
+let result =findLength([11,22,3,34,77,6])
+console.log(result)
