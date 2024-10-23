8d65f3afdc3566acddad644791e11a0ca83f5ecc
unknown <nahidujjamanraj@gmail.com>
2024-10-23 09:33:23 +0600

    I write a function that convert into kilometer into Miles

---
+++ 65.js
@@ -0,0 +1,7 @@
+// write a function that convert into kilometer into Miles
+
+function conversion(kilometer){
+    let conversion_Kilometer_Miles=kilometer / 1.60934
+    console.log(conversion_Kilometer_Miles)
+}
+conversion(35.40548)
