73e6b3d1083473d423516df33e758362a3c13de0
unknown <nahidujjamanraj@gmail.com>
2024-10-23 18:55:30 +0600

    In this program I write a function that convert temperature from Celsius to Fahrenheit

---
+++ 71.js
@@ -0,0 +1,8 @@
+// Write a function to convert temperature from Celsius to Fahrenheit.
+function Celsius_to_Fahrenheit(Celsius_value){
+    let Fahrenheit=(9*Celsius_value)/5+32;
+    return Fahrenheit
+
+}
+let Fahrenheit_result=Celsius_to_Fahrenheit(32)
+console.log(Fahrenheit_result,"°F")
