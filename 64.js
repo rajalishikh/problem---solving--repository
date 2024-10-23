745fc03396c31faa6945dd8ca62d81de4a3b722b
unknown <nahidujjamanraj@gmail.com>
2024-10-23 09:23:38 +0600

    In this program i write a function that convert into Miles into kilometer

---
+++ 64.js
@@ -0,0 +1,7 @@
+// write a function that convert into Miles into kilometer
+function conversion(Miles){
+    let conversion_miles_kilometer=Miles *1.60934;
+    return conversion_miles_kilometer;
+}
+let output=conversion(22);
+console.log("The total way is ",+output+' kilometer')
