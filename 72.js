4974731394c80bebd2d02ee4f4a5032515300b67
unknown <nahidujjamanraj@gmail.com>
2024-10-24 11:25:48 +0600

    In this program I Write a function to count the number of vowels in a string

---
+++ 72.js
@@ -0,0 +1,20 @@
+// Write a function to count the number of vowels in a string.
+function count(vowels){
+    let vowels_lowercase=vowels.toLowerCase();
+    console.log(vowels_lowercase)
+    let counter=''
+    for(let i=0;i<vowels.length;i++){
+        let vowels_lowercase=vowels[i].toLowerCase();
+        if(vowels_lowercase == 'a' || vowels_lowercase == 'e' || vowels_lowercase == 'i' || vowels_lowercase == 'o' || vowels_lowercase == 'u'){
+            counter=counter+vowels_lowercase;
+           
+        }
+
+       
+    }
+    console.log(counter)
+    return counter.length
+    
+}
+ let total_variable_vowel= count('I am a junior web developer')
+ console.log(total_variable_vowel)
---
+++ 73.js
@@ -0,0 +1,13 @@
+// Make function that make lottery system 
+function lottery(candidate){
+    for(let i=0; i<candidate.length;i++){
+        
+        let random=Math.ceil(Math.random()*candidate.length)
+        
+       
+
+       
+    }
+
+}
+lottery([1,2,3,5,6,7,8])
