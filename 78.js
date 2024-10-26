220557fe80a3576e59c6be55a044f25908b0c8bd
unknown <nahidujjamanraj@gmail.com>
2024-10-26 13:57:35 +0600

    I write a program  find the highest number in the array

---
+++ 78.js
@@ -0,0 +1,12 @@
+//  find the highest number in the array 
+let max_number=[22,33,66,99,22, ]
+function highest_number(x){
+    let highest_number_formula=Math.max(...x)
+    return highest_number
+}
+let result=highest_number(max_number)
+console.log(result)
+
+
+
+ 
