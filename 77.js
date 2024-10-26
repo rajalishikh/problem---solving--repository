78c88579dee8a1f9d3dde1f0ab26f2901b2f949f
unknown <nahidujjamanraj@gmail.com>
2024-10-26 13:22:59 +0600

    In this program I make function that treat the hihest mark founded in exam

---
+++ 77.js
@@ -0,0 +1,13 @@
+// find highest number  in the exam between three student 
+function highest(num1,num2,num3){
+    if(num1 > num2 && num1>num3  ){
+        console.log("I will give num1 a treat ")
+        
+    }else if( num2 > num1 && num2> num3 ){
+        console.log("I will give treat num 2 ")
+    }
+    else{
+        console.log('I wil give treat bushra ')
+    }
+}
+highest(20,30,55)
