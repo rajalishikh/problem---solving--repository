83cc7808efb633a42ae2760d46b9755e8cee5709
unknown <nahidujjamanraj@gmail.com>
2024-10-30 08:52:29 +0600

    In this function I make a  change .If parameter its a not number it will give a sms

--- 94.js
+++ 94.js
@@ -1,7 +1,13 @@
 function AnaToVori(ana){
-    let Convert_vori=ana * 0.0625;
-    return Convert_vori
+    if(typeof ana != 'number'){
+       return("please provided me a valid number ")
+    }else{
+        let Convert_vori=ana * 0.0625;
+        return Convert_vori
 
+
+    }
+    
 }
-let result=AnaToVori(22)
+let result=AnaToVori('number')
 console.log(result,"Vori")
